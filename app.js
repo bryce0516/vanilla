eb594ea349ac61837318b20f25cd7a9902dc0a69
bryce0516 <jayhspark@gmail.com>
2022-02-27 12:11:27 +0900

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(
+  "/static",
+  express.static(path.resolve(__dirname, "./src/static"), {
+    extensions: ["js", "css"],
+  })
+);
+
+const port = 3320;
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve("./src", "./index.html"));
+});
+
+app.listen(port, () => console.log("server is running on port : ", +port));
